87965e6efb1f9e22aa32913e88a9b4e731d8313c
teacher-mrdv <teacher-mrdv@users.noreply.github.com>
2020-03-22 18:09:53 +0800

    functional

---
+++ Collection/Collection.java
@@ -0,0 +1,61 @@
+
+public class Collection
+{
+	Element start;
+	Element current;
+	
+	public Collection()
+	{	this.start = null;
+		this.current = null;
+	}
+	
+	public boolean isEmpty()
+	{	return start == null;
+	}
+
+	public boolean hasNext()
+	{	return current != null;
+	}
+
+	public void addItem(String data)
+	// can also be a number, object, array, etc. instead of String
+	{
+		Element newElement = new Element(data);
+		if( isEmpty() ) // add first element
+		{	start = newElement;
+			current = start;
+		} else {
+			current.setNext(newElement);
+			current = current.getNext();
+		}
+	}
+	
+	public String getNext()
+	{
+		String data = current.getData();
+		current = current.getNext();
+		return data;
+	}
+	
+	public void resetNext()
+	{	this.current = this.start;
+	}
+	
+	public String toString()
+	{	String output = "[ ";
+		resetNext();
+		while( this.current != null )
+		{	String e = this.getNext();
+			output = output + e + "  ";
+		}
+		return output + "]";
+	}
+	
+	public void print()
+	{	this.resetNext();
+		while( this.current.hasNext() )
+		{	System.out.println(this.getNext());
+		}
+	}
+}
+
