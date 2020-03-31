cabd9a4d33a903d98b917359878ceecede7dcdc2
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2020-03-31 15:34:02 +0800

    Update SortingSkeletonHW.java

--- sort-search/SortingSkeletonHW.java
+++ sort-search/SortingSkeletonHW.java
@@ -92,7 +92,7 @@ public class SortingSkeletonHW
 	// No need to modify the main method.
 	public static void main(String[] args)
 	{
-		int[] original = { 7,9,6,8,1,3,5,2,4,3 };
+		int[] original = { 7,9,6,8,1,3,5,2,4 };
 		//int[] original = { 9,8,7,6,5,4,3,2,1 };
 		int[] array = clone(original);
 		System.out.println("Original array");
